ff07a6549fdedf7d34095934c7c6b91ec29d6a36
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-02-09 22:19:16 +0530

    Update crcsender.c

--- crcsender.c
+++ crcsender.c
@@ -1,36 +1,44 @@
 #include<stdio.h>
-#include<string.h>	
+#include<string.h>
+void crc(char dw[],char div[])
+{
+	char temp[50],rem[50];
+	int dl=strlen(dw),i,j;
+	int divl=strlen(div);
+	strcpy(temp,dw);
+	for(i=0;i<divl-1;i++)
+		dw[dl+i]='0';
+	dw[dl+divl-1]='\0';
+	div[divl]='\0';
+	printf("After zero padding, the updated dividend is: ");
+	puts(dw);
+	for(i=0;i<dl;i++)
+	{
+		if(dw[i]=='0')
+			continue;
+		else{
+		for(j=0;j<divl;j++)
+		{
+			dw[i+j]=(dw[i+j]==div[j])?'0':'1';
+		}
+		}
+		}
+	printf("CRC bit: ");
+	for(i=strlen(dw)-divl+1;i<strlen(dw);i++)
+		printf("%c",dw[i]);
+	printf("\n");
+	for(i=0;i<dl;i++)
+		dw[i]=temp[i];
+	printf("Transmitted bit pattern : ");
+	puts(dw);
+}
+	
 void main()
 {
-	char dw[50],div[50],rem[50],temp[50];
+	char dw[50],div[50];
 	printf("Enter the dataword: ");
 	scanf("%s",dw);
 	printf("Enter the divisor: ");
 	scanf("%s",div);
-	int dl=strlen(dw),i,j;
-	int divl=strlen(div);
-	dw[dl]='\0';
-	div[divl]='\0';
-	strcpy(temp,dw);
-	for(i=0;i<divl-1;i++)
-		temp[dl+i]='0';
-	printf("After padding n+1 zeros, the dataword is[here n = %d] : ",divl);
-	puts(temp);
-	for(i=0;i<dl;i++)
-	{
-		if(temp[i]!='0'){
-			for(j=0;j<divl;j++)
-				rem[j]=temp[i+j]=(temp[i+j]==div[j])?'0':'1';
-			}
-	/*printf("%s\n",temp);
-	printf("%s\n",rem);*/
-	}
-	for(i=0;i<divl;i++)
-		rem[i]=rem[i+1];
-	printf("CRC bit: ");
-	puts(rem);
-	strcat(dw,rem);
-	printf("The transmitted bit pattern: ");
-	puts(dw);
+	crc(dw,div);
 }
-
